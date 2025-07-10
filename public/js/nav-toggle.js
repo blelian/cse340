56825ff7b0050f7b0b62e5eeba2d94e56e7ce206
blelian <bullsurus@gmail.com>
2025-07-10 13:18:34 +0200

    made it work

---
+++ public/js/nav-toggle.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navToggle = document.getElementById("nav-toggle");
+  const mainNav = document.getElementById("main-nav");
+
+  console.log("Nav Toggle:", navToggle);
+  console.log("Main Nav:", mainNav);
+
+  if (navToggle && mainNav) {
+    navToggle.addEventListener("click", () => {
+      mainNav.classList.toggle("open");
+      console.log("Toggle clicked! Current classes:", mainNav.classList);
+    });
+  } else {
+    console.error("navToggle or mainNav not found");
+  }
+});
--- server.js
+++ server.js
@@ -5,15 +5,31 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
+
+const app = express();
+
+const staticRoutes = require("./routes/static");
+
+
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout")//not at views root
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(staticRoutes);
+//Index Route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
